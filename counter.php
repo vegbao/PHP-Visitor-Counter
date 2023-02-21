92e7f17fa77c312a73f9ce5fbcc2dfb8e31ab5ab
菜包 <36061682+vegbao@users.noreply.github.com>
2023-02-21 22:10:08 +0800

    Update the English description

--- counter.php
+++ counter.php
@@ -1,25 +1,41 @@
 <?php
+// PHP Visitor Counter by vegbao 2023.
+// https://github.com/vegbao/PHP-Visitor-Counter
+// ---------------------------------------------------------------
+// Retro-style website counter that requires no database,
+// uses plain text storage, and can display today's, yesterday's,
+// and total visitors in either text or image format.
+// 純文字免資料庫 PHP 文字、圖片網站計數器，可顯示今天、昨天、累計人次。
+// ---------------------------------------------------------------
+
+// Store the counter data in a JSON file
 // 設定計數器儲存的 JSON 檔案路徑
 $counterFile = 'counter.json';
 
+// Display visitor count for today, yesterday, and total as text or images
 // 顯示今天、昨天、累計的文字（或圖片）
-$txt_t = '今日：';  // Today
-$txt_y = '昨日：';  // Yesterday
-$txt_a = '累計：';  // Total
-$line =  ' / '; // 分隔線（也可使用<br>換行）
+$txt_t = 'Today:';  // 今天
+$txt_y = 'Yesterday:';  // 昨天
+$txt_a = 'Total:';  // 累計
+$line =  ' / '; // 分隔線（也可使用<br>換行） // Add a separator line or line break(<br>)
 
+// Choose number of digits and add leading zeros if needed
 // 設定顯示數字的位數，如果數字不足這個位數，就會在前面補 0
-$digitCount = 3;    // e.g. 007
+$digitCount = 3;
 
+// Choose text-only(txt) or image display(img)
 // 設定顯示計數器的方式，txt 代表純文字顯示，img 代表使用圖片顯示
 $display_counter = 'txt';
 
+// Choose image's folder
 // 設定數字圖片的儲存資料夾（如果設為圖片）
 $imgDir = 'img/';
 
+// Choose image format for numbers
 // 設定數字圖片的格式
 $imgFormat = '.png';
 
+// If JSON file doesn't exist, create it
 // 檢查 JSON 檔案是否存在，如果不存在就建立
 if (!file_exists($counterFile)) {
     $data = [
@@ -32,10 +48,12 @@ if (!file_exists($counterFile)) {
     file_put_contents($counterFile, $jsonData);
 }
 
+// Read data from JSON file
 // 讀取 JSON 檔案的資料
 $jsonData = file_get_contents($counterFile);
 $data = json_decode($jsonData, true);
 
+// Update yesterday's count and reset today's count if it's not the same day
 // 檢查是否是同一天，如果不是，就把昨天的訪問量更新到 $data['yesterday']，並重置今天的訪問量
 $currentDate = date('Y-m-d');
 if ($currentDate !== $data['date']) {
@@ -44,14 +62,17 @@ if ($currentDate !== $data['date']) {
     $data['date'] = $currentDate;
 }
 
+// Increment visitor count
 // 訪問量 +1
 $data['today']++;
 $data['total']++;
 
+// Update data in JSON file
 // 更新 JSON 檔案的資料
 $jsonData = json_encode($data);
 file_put_contents($counterFile, $jsonData);
 
+// Display the result
 // 顯示結果
 echo '<div style="text-align: center">';
 if ($display_counter === 'txt') {
@@ -68,6 +89,7 @@ if ($display_counter === 'txt') {
 }
 echo '</div>';
 
+// Use this function to show number images.
 // 輸出數字圖片的函數
 function numImg($number, $imgDir, $imgFormat, $digitCount)
 {
